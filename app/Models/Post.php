8a280fbd7baa40a60adbc954abc180dc54e36701
ouissal-edd <ouissal.edouj21@gmail.com>
2021-07-09 14:50:36 +0100

    lastcomit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,10 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+
+    public function ownedBy(User $user)
+    {
+        return $user->id === $this->user_id;
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,21 +28,27 @@
             <a href="" class="font-bold">{{$post->user->name}}</a>
             <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
             <p class="mb-2">{{$post->body}}</p>
+
+
+            @if($post->ownedBy(auth()->user()))
+            <div>
+                <form action="{{route('posts.destroy',$post)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-blue-500 ">Delete</button>
+                </form>
+            </div>
+            @endif
+
+
+            @endforeach
+            {{$posts->links()}}
+            @else
+            <p>There are no posts</p>
+            @endif
+
+
+
         </div>
-        <form action="{{route('posts.destroy',$post)}}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="text-blue-500 ">Delete</button>
-        </form>
-
-        @endforeach
-        {{$posts->links()}}
-        @else
-        <p>There are no posts</p>
-        @endif
-
-
-
     </div>
-</div>
-@endsection
+    @endsection
